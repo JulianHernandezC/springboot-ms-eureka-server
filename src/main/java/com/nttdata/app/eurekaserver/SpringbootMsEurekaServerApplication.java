8f615401423edb4fac06baea3cdaf09f8e152f56
Julián <julian.hernandez.carrion@nttdata.com>
2022-03-18 10:26:07 +0100

    pom.xml

---
+++ src/main/java/com/nttdata/app/eurekaserver/SpringbootMsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.app.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsEurekaServerApplication.class, args);
+	}
+
+}
